0ac344abb00d521352bcb60ccae4559e6bc5561a
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-04-07 19:27:32 +0100

    added frontend log in modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,44 @@
 import { MdAdd } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../atoms/authModal";
+import { useState } from "react";
+import AuthModal from "./User/AuthModal";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
 
     const handleAddEvent = () => {
         // Add check user logic
-        navigate("/add-event")
+        if (!userLoggedIn) {
+            setAuthModalState({
+                open: true,
+                view: "login"
+            });
+        } else {
+            navigate("/add-event")
+        }
     }
 
     return (
-        <div className="w-full px-6 sm:px-10 py-4 md:py-6 border-b border-gray-200">
-            <div className="max-w-7xl flex flex-row items-center justify-between mx-auto">
-                <Link to="/">
-                    <p className="tracking-wide text-lg font-medium md:text-xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-rose-400">Queer ESEA Events</p>
-                </Link>
-                <div className="bg-white rounded-full p-3 cursor-pointer hover:bg-indigo-400 hover:text-white">
-                    <MdAdd
-                        className="text-xl"
-                        onClick={handleAddEvent}
-                    />
+        <>
+            <AuthModal />
+            <div className="w-full px-6 sm:px-10 py-4 md:py-6 border-b border-gray-200">
+                <div className="max-w-7xl flex flex-row items-center justify-between mx-auto">
+                    <Link to="/">
+                        <p className="tracking-wide text-lg font-medium md:text-xl text-transparent bg-clip-text bg-gradient-to-r from-indigo-600 to-rose-400">Queer ESEA Events</p>
+                    </Link>
+                    <div className="bg-white rounded-full p-3 cursor-pointer hover:bg-indigo-400 hover:text-white">
+                        <MdAdd
+                            className="text-xl"
+                            onClick={handleAddEvent}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
---
+++ src/components/User/AuthModal.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { authModalState } from "../../atoms/authModal"
+import { useRecoilState } from "recoil";
+
+const AuthModal = () => {
+    const [modalState, setModalState] = useRecoilState(authModalState);
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleClose = () => {
+        setModalState(prev => ({
+            ...prev,
+            open: false,
+        }))
+    }
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setUser({
+            ...user,
+            [name]: value,
+        })
+    }
+
+    return (
+        <>
+            {modalState.open && (
+                <div className="max-w-4xl mx-auto flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none bg-gray-100/40 focus:outline-none">
+                    <div className="my-5 mx-auto w-4/5 sm:w-1/2 bg-white flex flex-col rounded-xl shadow-lg outline-none">
+                        <div className="flex flex-row items-center justify-between mx-4 px-4 py-10 border-b">
+                            <p className="text-2xl lg:text-3xl font-semibold text-gray-500 tracking-wider">Log In</p>
+                            <div
+                                className="cursor-pointer"
+                                onClick={handleClose}
+                            >
+                                <p className="text-2xl text-gray-400 font-bold pr-4 hover:text-amber-500">✕</p>
+                            </div>
+                        </div>
+                        <form className="py-6 px-10">
+                            <div className="w-full mx-auto flex flex-col gap-6 py-10">
+                                <div className="w-full flex flex-col gap-2">
+                                    <label
+                                        htmlFor="email"
+                                        className="text-gray-600 font-bold uppercase tracking-wide"
+                                    >
+                                        Email Address
+                                    </label>
+                                    <input
+                                        className="w-full bg-gray-200 text-gray-600 md:text-lg border border-gray-200 p-4 leading-tight rounded-lg appearance-none focus:bg-white focus:border-gray-500"
+                                        id="email"
+                                        name="email"
+                                        type="text"
+                                        placeholder="example@email.com"
+                                        autoComplete="example@email.com"
+                                        onChange={handleInput}
+                                        required
+                                    />
+                                </div>
+                                <div className="w-full flex flex-col gap-2">
+                                    <label
+                                        htmlFor="password"
+                                        className="text-gray-600 font-bold uppercase tracking-wide mb-2"
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        className="w-full bg-gray-200 text-gray-600 md:text-lg border border-gray-200 p-4 leading-tight rounded-lg appearance-none focus:bg-white focus:border-gray-500"
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        placeholder="***********"
+                                        autoComplete="current-password"
+                                        onChange={handleInput}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-center justify-center pt-10 pb-6 px-1 border-t boder-slate-200">
+                                <button className="mx-auto bg-violet-400 text-white font-bold uppercase px-8 py-3 rounded-lg shadow hover:shadow-lg hover:opacity-90 outline-none tracking-wide ease-linear transition-all duration-150">Log In</button>
+                                <p className="text-teal-600 text-center pt-5 cursor-pointer hover:underline underline-offset-4 decoration-2 decoration-teal-500">Request to become a member</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default AuthModal
